ee295b019e3e82c1cc70d714f481d46f0b6e5180
Mike Conroy <mikecon@outlook.com>
2020-11-30 23:08:11 +0000

    Matrix Product with a batch of inputs.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import numpy as np
 
-inputs = [1.0, 2.0, 3.0, 2.5]
+#Batch of inputs (matrix).
+#Shape - (3, 4)
+inputs = [[1.0, 2.0, 3.0, 2.5],
+	  [2.0, 5.0, -1.0, 2.0],
+	  [-1.5, 2.7, 3.3, -0.8]]
+#Shape - (3,4)
 weights = [[0.2, 0.8, -0.5, 1.0],
 	  [0.5, -0.91, 0.26, -0.5],
 	  [-0.26, -0.27, 0.17, 0.87]]
 biases = [2.0, 3.0, 0.5]
 
-# np.dot() - hanldes the 2D array and returns a vector of the same size
-# (3 in this case)
-# np.dot(weights, inputs) = [np.dot(weights[0], inputs), np.dot(weights[1], inputs) etc.]
-# Whatever comes first in np.dot will determine the output shape.
-# np.dot((3, 4), 4) = output vector of (3)
-outputs = np.dot(weights, inputs) + biases
+#Matrix Product requires the no. of rows of matrix 1 (3)
+#to match no. of columns of matrix 2 (4)
+#Index 1 of matrix 1.
+#Index 0 of matrix 2.
+#We transpose so they do (1 weight per input)
+#Matrix Product - multiply each value in row of matrix 1
+#by each value in column of matrix 2.
+layer_outputs = np.dot(inputs, np.array(weights).T) + biases
 
-print(outputs)
+#Output is a list of layer outputs per sample.
+#This is why inputs is first parameter.
+#Sample related output so it can be passed into the next layer.
+print(layer_outputs)
