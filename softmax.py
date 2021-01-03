ff9d7b3e81a6a38b77c7f2db61bb5e18db446597
Mike Conroy <mikecon@outlook.com>
2021-01-03 21:07:15 +0000

    Convert Softmax example to Numpy

--- softmax.py
+++ softmax.py
@@ -1,23 +1,15 @@
 import math
+import numpy as np
 
-# Values from previous output
+# Values from previous layer's output
 layer_outputs = [4.8, 1.21, 2.385]
 
-# e - math constant.
-E = math.e
-
 # calculate exponential value for each value in vector.
-exp_values = []
-for output in layer_outputs:
-    exp_values.append(E ** output)
+exp_values = np.exp(layer_outputs)
 print("Exponentiated Values:", exp_values)
 
 # Normalize the values
-norm_base = sum(exp_values)
-norm_values = []
-
-for value in exp_values:
-    norm_values.append(value / norm_base)
+norm_values = exp_values / sum(exp_values)
 
 print("Normalized exponentiated values:", norm_values)
-print("Sum of normalized values:", sum(norm_values))
+print("Sum of normalized values:", np.sum(norm_values))
