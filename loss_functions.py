50aa6565ebef035d9962f442fecc7c333a6f3f8a
Mike Conroy <mikecon@outlook.com>
2021-01-06 22:35:55 +0000

    Initial example of cross-entropy loss function.

---
+++ loss_functions.py
@@ -0,0 +1,13 @@
+import math
+
+# Example output from output layer of NN
+softmax_output = [0.7, 0.1, 0.2]
+
+# Ground Truth
+target_output = [1, 0, 0]
+
+loss = -(math.log(softmax_output[0])*target_output[0] +
+        math.log(softmax_output[1])*target_output[1] +
+        math.log(softmax_output[2])*target_output[2])
+
+print(loss)
