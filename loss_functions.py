3426af28baaa0ca5accf6d03756801605684d68d
Mike Conroy <mikecon@outlook.com>
2021-01-06 23:04:00 +0000

    Update categorical crosss entropy with NumPy functions to calculate negative log and average loss of batches.

--- loss_functions.py
+++ loss_functions.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 # Example output from output layer of NN
 softmax_output = [0.7, 0.1, 0.2]
@@ -15,5 +16,39 @@ target_output = [1, 0, 0]
 # The 0 classes will multiply to 0 so not required.
 # This means we just need:
 loss = -math.log(softmax_output[0])
-
 print(loss)
+
+# Probabilities for 3 samples.
+# Classes = Dog, Cat, Human
+softmax_outputs = np.array([[0.7, 0.1, 0.2],
+                            [0.1, 0.5, 0.4],
+                            [0.02, 0.9, 0.08]])
+class_targets = [0, 1, 1] #Dog, Cat, Cat
+
+print("ZIP Function:")
+for targ_idx, distribution in zip(class_targets, softmax_outputs):
+    print(distribution[targ_idx])
+
+# NP let's us simplify this:
+print("NumPy:")
+print(softmax_outputs[[0, 1, 2], class_targets])
+
+# We can then use range to go through full batch without hard-coding indices.
+print("NumPy - Dynamic Batch Size:")
+print(softmax_outputs[
+    range(len(softmax_outputs)), class_targets
+])
+
+# Now Apply negative log to each item:
+print("With Negative Log:")
+print(-np.log(softmax_outputs[
+    range(len(softmax_outputs)), class_targets
+]))
+
+# Calculate Average Loss
+neg_log = -np.log(softmax_outputs[
+            range(len(softmax_outputs)), class_targets
+          ])
+average_loss = np.mean(neg_log)
+print("Average Loss:")
+print(average_loss)
