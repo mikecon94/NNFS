785ed07d75307cdcc9cc523fccaa0469046df7cd
Mike Conroy <mikecon@outlook.com>
2021-01-06 22:38:34 +0000

    Simplify categorical cross-entropy.

--- loss_functions.py
+++ loss_functions.py
@@ -6,8 +6,14 @@ softmax_output = [0.7, 0.1, 0.2]
 # Ground Truth
 target_output = [1, 0, 0]
 
-loss = -(math.log(softmax_output[0])*target_output[0] +
-        math.log(softmax_output[1])*target_output[1] +
-        math.log(softmax_output[2])*target_output[2])
+# loss = -(math.log(softmax_output[0])*target_output[0] +
+#         math.log(softmax_output[1])*target_output[1] +
+#         math.log(softmax_output[2])*target_output[2])
+
+# For categorical cross-entropy
+# 1 class will be set to 1 and the rest 0.
+# The 0 classes will multiply to 0 so not required.
+# This means we just need:
+loss = -math.log(softmax_output[0])
 
 print(loss)
