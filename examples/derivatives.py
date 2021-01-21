8f9d08dce982c78f794ccd6390267268f8e55178
Mike Conroy <mikecon@outlook.com>
2021-01-21 22:26:56 +0000

    Numerical Derivative.

--- examples/derivatives.py
+++ examples/derivatives.py
@@ -47,6 +47,36 @@ def tangent_line(x):
 # +/- 0.9 to draw the tangent line on the graph
 # Then calculate the y for given x using the tangent line function
 to_plot = [x1-0.9, x1, x1+0.9]
-plt.plot(to_plot, [tangent_line(i) for i in to_plot])
+# plt.plot(to_plot, [tangent_line(i) for i in to_plot])
 print('Approximate derivative for f(x)', f'where x = {x1} is {approximate_derivative}')
+
+x = np.array(np.arange(0, 5, 0.001))
+y = f(x)
+
+plt.plot(x, y)
+colors = ['k', 'g', 'r', 'b', 'c']
+
+def approximate_tangent_line(x, approximate_derivative):
+    return (approximate_derivative * x) + b
+
+for i in range(5):
+    p2_delta = 0.0001
+    x1 = i
+    x2 = x1 + p2_delta
+    y1 = f(x1)
+    y2 = f(x2)
+    print((x1, y1), (x2, y2))
+
+    approximate_derivative = (y2 - y1) / (x2 - x1)
+    b = y2 - (approximate_derivative * x2)
+
+    to_plot = [x1 - 0.9, x1, x1 + 0.9]
+
+    plt.scatter(x1, y1, c=colors[i])
+    plt.plot([point for point in to_plot],
+              [approximate_tangent_line(point, approximate_derivative)
+                for point in to_plot],
+              c=colors[i])
+    print('Approximate derivative for f(x)', f'where x = {x1} is {approximate_derivative}')
+
 plt.show()
