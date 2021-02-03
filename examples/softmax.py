34c162e7cf37d4a0ffbc97fcd5079c5744bb9255
Mike Conroy <mikecon@outlook.com>
2021-02-03 22:40:02 +0000

    Add backpropagation derivative for softmax

--- examples/softmax.py
+++ examples/softmax.py
@@ -68,3 +68,12 @@ print(softmax.output)
 
 
 # BACKWARDS PASS - PAGE 226
+print("BACKWARDS PASS")
+softmax_output = [0.7, 0.1, 0.2]
+softmax_output = np.array(softmax_output).reshape(-1, 1)
+print(softmax_output)
+print(np.eye(softmax_output.shape[0]))
+print(softmax_output * np.eye(softmax_output.shape[0]))
+print(np.diagflat(softmax_output))
+print(np.dot(softmax_output, softmax_output.T))
+print(np.diagflat(softmax_output) - np.dot(softmax_output, softmax_output.T))
--- main.py
+++ main.py
@@ -47,7 +47,23 @@ class Activation_Softmax:
         # Normalize them for each sample
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
-        
+    
+    def backward(self, dvalues):
+        # Create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+            # Flatten Output Array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - \
+                              np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
 # Base/Common Loss Class
 class Loss:
     # Calculates the data and regularization losses.
