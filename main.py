51ecef9324b4a2f61fc44e15ce4f1d0dbe001acd
Mike Conroy <mikecon@outlook.com>
2021-03-25 18:40:22 +0000

    Add Adam Optimizer

--- main.py
+++ main.py
@@ -271,6 +271,71 @@ class Optimizer_RMSprop:
     def post_update_params(self):
         self.iterations += 1
 
+class Optimizer_Adam:
+
+    # Initialize optimizer - set settings,
+    def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, beta_1=0.9, beta_2=0.999):
+        self.learning_rate = learning_rate
+        self.decay = decay
+        self.current_learning_rate = learning_rate
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+    
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * \
+                        (1. / (1. + self.decay * self.iterations))
+
+    def update_params(self, layer):
+
+        if not hasattr(layer, 'weight_momentums'):
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+        
+        # Update momentum with current gradients
+        layer.weight_momentums = self.beta_1 * layer.weight_momentums + \
+                                 (1 - self.beta_1) * layer.dweights
+        layer.weight_momentums = self.beta_1 * layer.bias_momentums + \
+                                 (1 - self.beta_1) * layer.dbiases
+
+        # Get corrected Momentum
+        # self.iteration is 0 at first pass
+        # and we need to start with 1 here                     
+        weight_momentums_corrected = layer.weight_momentums / \
+                                    (1 - self.beta_1 ** (self.iterations +1))
+        bias_momentums_corrected = layer.bias_momentums / \
+                                   (1 - self.beta_1 ** (self.iterations +1))
+
+        # Update cache with square current gradientss
+        layer.weight_cache = self.beta_2 * layer.weight_cache + \
+                             (1 - self.beta_2) * layer.dweights**2
+        layer.bias_cache = self.beta_2 * layer.bias_cache + \
+                             (1 - self.beta_2) * layer.dbiases**2
+
+        # Get corrected cache
+        weight_cache_corrected = layer.weight_cache / \
+                                 (1 - self.beta_2 ** (self.iterations + 1))
+        bias_cache_corrected = layer.bias_cache / \
+                                 (1 - self.beta_2 ** (self.iterations + 1))
+
+        # Vanilla SGD parameter update + normalization
+        # With square rooted cache
+        layer.weights += -self.current_learning_rate * \
+                         weight_momentums_corrected / \
+                         (np.sqrt(weight_cache_corrected) + self.epsilon)
+        layer.biases += -self.current_learning_rate * \
+                        bias_momentums_corrected / \
+                        (np.sqrt(bias_cache_corrected) + self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
+
 # Create dataset
 # X is coordinates
 # y is the class
@@ -291,7 +356,7 @@ loss_activation = Activation_Softmax_Loss_CategoricalCrossEntropy()
 
 # Create Optimizer
 # optimizer = Optimizer_SGD(decay=1e-3, momentum=0.85)
-optimizer = Optimizer_RMSprop(learning_rate=0.02, decay=1e-4, rho=0.899)
+optimizer = Optimizer_Adam(learning_rate=0.02, decay=1e-5)
 
 # Train in loop
 for epoch in range(10001):
