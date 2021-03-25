8bc63c7079e209b7dee1df13797f1da2a7e26f18
Mike Conroy <mikecon@outlook.com>
2021-03-25 18:55:46 +0000

    Fix Adam Optimizer

--- main.py
+++ main.py
@@ -272,7 +272,6 @@ class Optimizer_RMSprop:
         self.iterations += 1
 
 class Optimizer_Adam:
-
     # Initialize optimizer - set settings,
     def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, beta_1=0.9, beta_2=0.999):
         self.learning_rate = learning_rate
@@ -283,25 +282,23 @@ class Optimizer_Adam:
         self.beta_1 = beta_1
         self.beta_2 = beta_2
     
+    # Call once before any parameter updates
     def pre_update_params(self):
         if self.decay:
             self.current_learning_rate = self.learning_rate * \
-                        (1. / (1. + self.decay * self.iterations))
+                                        (1. / (1. + self.decay * self.iterations))
 
     def update_params(self, layer):
-
-        if not hasattr(layer, 'weight_momentums'):
-            layer.weight_momentums = np.zeros_like(layer.weights)
-            layer.bias_momentums = np.zeros_like(layer.biases)
-
         if not hasattr(layer, 'weight_cache'):
+            layer.weight_momentums = np.zeros_like(layer.weights)
             layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
             layer.bias_cache = np.zeros_like(layer.biases)
         
         # Update momentum with current gradients
         layer.weight_momentums = self.beta_1 * layer.weight_momentums + \
                                  (1 - self.beta_1) * layer.dweights
-        layer.weight_momentums = self.beta_1 * layer.bias_momentums + \
+        layer.bias_momentums = self.beta_1 * layer.bias_momentums + \
                                  (1 - self.beta_1) * layer.dbiases
 
         # Get corrected Momentum
