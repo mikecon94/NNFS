6fde19456edccc747630ca05782b179fc1068bfc
Mike Conroy <mikecon@outlook.com>
2021-03-25 16:46:09 +0000

    AdaGrad optimizer.

--- main.py
+++ main.py
@@ -192,6 +192,44 @@ class Optimizer_SGD:
     def post_update_params(self):
         self.iterations += 1
 
+class Optimizer_Adagrad:
+    # Initialize optimizer - set settings,
+    # learning rate of 1. Default.
+    def __init__(self, learning_rate=1., decay=0., momentum=0., epsilon=1e-7):
+        self.learning_rate = learning_rate
+        self.decay = decay
+        self.current_learning_rate = learning_rate
+        self.iterations = 0
+        self.epsilon = epsilon
+    
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * \
+                        (1. / (1. + self.decay * self.iterations))
+
+    def update_params(self, layer):
+
+        # If layer doesn't contain cache arrays, create them filled with zeroes.
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+        
+        # Update cache with squared current gradients
+        layer.weight_cache += layer.dweights ** 2
+        layer.bias_cache += layer.dbiases ** 2
+
+        # Vanilla SGD parameter update + normalization
+        # With square rooted cache
+        layer.weights += -self.current_learning_rate * \
+                         layer.dweights / \
+                         (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * \
+                        layer.dbiases / \
+                        (np.sqrt(layer.bias_cache) + self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
+
 # Create dataset
 # X is coordinates
 # y is the class
@@ -211,7 +249,8 @@ dense2 = Layer_Dense(64, 3)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossEntropy()
 
 # Create Optimizer
-optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
+# optimizer = Optimizer_SGD(decay=1e-3, momentum=0.85)
+optimizer = Optimizer_Adagrad(decay=1e-4)
 
 # Train in loop
 for epoch in range(10001):
