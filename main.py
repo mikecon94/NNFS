e04a8ce4b447af4c83a7466ae7ed4ed6f127d5d5
Mike Conroy <mikecon@outlook.com>
2021-04-02 17:56:14 +0100

    Add Binary Cross Entropy Loss function.

--- main.py
+++ main.py
@@ -199,6 +199,39 @@ class Loss_CategoricalCrossEntropy(Loss):
         # Normalize gradient
         self.dinputs = self.dinputs / samples
 
+class Loss_BinaryCrossEntropy(Loss):
+
+    def forward(self, y_pred, y_true):
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Calculate sample-wise loss
+        sample_losses = -(y_true * np.log(y_pred_clipped) + \
+                          (1 - y_true) * np.log(1 - y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis=-1)
+
+        return sample_losses
+
+    def backward(self, dvalues, y_true):
+        # Number of samples
+        samples = len(dvalues)
+
+        # Number of outputs in every sample
+        # We'll use the first sample to count them
+        outputs = len(dvalues[0])
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        clipped_dvalues = np.clip(dvalues, 1e-7, 1-1e-7)
+
+        # Calculate Gradient
+        self.dinputs = -(y_true / clipped_dvalues - \
+                         (1 - y_true) / (1 - clipped_dvalues)) / outputs
+        
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
 # Softmax Classifier - Combined Softmax activation
 # with cross-entropy loss for faster backward step.
 class Activation_Softmax_Loss_CategoricalCrossEntropy():
