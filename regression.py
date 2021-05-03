9f62c91fcfef36568107e0b90eb11c71099526b1
Mike Conroy <mikecon@outlook.com>
2021-05-03 13:43:47 +0100

    Optimize weights initialisation of dense layer (0.01 -> 0.1)

--- regression.py
+++ regression.py
@@ -21,7 +21,7 @@ class Layer_Dense:
         # np.random.randn produces a Gaussian distribution with a mean of 0 and variance of 1.
         # We multiply by 0.01 to reduce the numbers as otherwise the model will take more time
         # to fit the data during training and the starting values will be disproprtionately large.
-        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
         # Initialize a row vector of zeroes for weights.
         self.biases = np.zeros((1, n_neurons))
         # Self Regularization Strength
@@ -537,7 +537,6 @@ optimizer = Optimizer_Adam(learning_rate=0.005, decay=1e-3)
 # We'll calculate this precision as a fraction of standard deviation
 # of all the ground truth values
 accuracy_precision = np.std(y) / 250
-
 
 # Train in loop
 for epoch in range(10001):
